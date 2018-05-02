8eae722a6a7ec52291d5ecdedded57b675c9ac31
Anna <annakosiorek4@gmail.com>
2018-05-02 18:33:33 +0200

    first heatmap

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-import csv
 import matplotlib.pyplot as plt
-import numpy as np;
 import seaborn as sns
-from sklearn.datasets import make_classification
-
-fraud = []
-test = []
-with open("data_for_student_case.csv") as csv_file:
-    transactions = csv.DictReader(csv_file)
-    for row in transactions:
-        #FORMAT: txid,bookingdate,issuercountrycode,txvariantcode,bin,amount,currencycode,shoppercountrycode,shopperinteraction,simple_journal,cardverificationcodesupplied,cvcresponsecode,creationdate,accountcode,mail_id,ip_id,card_id
-        fraud.append(row)
-
-        if row['simple_journal'] == 'Settled':
-            test.append([float(row['cvcresponsecode']), 0, float(row['amount'])])
-        elif row['simple_journal'] == 'Chargeback':
-            test.append([float(row['cvcresponsecode']), 1, float(row['amount'])])
-        else:
-            pass
+import pandas as pd
 
 
-print(test)
-#fraudHeat = sns.load_dataset(test)
-#fraudHeat = fraudHeat.pivot('card_id','simple_journal','amount')
-ax = sns.heatmap(test)
+
+df = pd.read_csv('data_for_student_case.csv', index_col=0) #dataframe
+df['cvcresponsecode'].apply(lambda x: float(x))
+df['amount'].apply(lambda x: float(x))
+df['simple_journal'], labels = pd.factorize(df.simple_journal)
+
+stats_df = df[['cvcresponsecode', 'simple_journal', 'amount']]
+stats_df = stats_df.groupby(['cvcresponsecode', 'simple_journal'], as_index=False)['amount'].mean()#['amount'].agg('sum')
+pivot = stats_df.pivot(index='cvcresponsecode', columns='simple_journal', values='amount')
+print(labels)
+sns.heatmap(pivot)
+
 plt.show()
 
 #make_classification()
+
