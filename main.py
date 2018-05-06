fe39c58743480465dc9b57185ef37c834204b4f6
marius <marius.pop94@gmail.com>
2018-05-06 20:43:18 +0200

    Smote finished!

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
-import re
 
 #FORMAT: txid,bookingdate,issuercountrycode,txvariantcode,bin,amount,currencycode,shoppercountrycode,shopperinteraction,simple_journal,cardverificationcodesupplied,cvcresponsecode,creationdate,accountcode,mail_id,ip_id,card_id
 
@@ -60,19 +59,28 @@ def smote(df):
     df = pd.get_dummies(df, columns=['issuercountrycode', 'txvariantcode', 'currencycode', 'shoppercountrycode',
                                      'shopperinteraction', 'accountcode', 'cardverificationcodesupplied'])
 
-
-    X = df.drop(['simple_journal', 'bookingdate', 'mail_id', 'ip_id', 'card_id'], axis=1).values
     y = df['simple_journal'].values
+    df = df.drop(['simple_journal', 'bookingdate', 'mail_id', 'ip_id', 'card_id'], axis=1)
+    X = df.values
+
     print(np.shape(X), np.shape(y))
 
 
     # SMOTE Data
     sm = SMOTE(random_state=15)
     X_resampled, y_resampled = sm.fit_sample(X, y)
-    # print(sorted(Counter(y_resampled).items()))
-    print(X_resampled)
-    # pd.to_csv(df2)
 
+    df2 = pd.DataFrame(X_resampled,columns=df.columns.values)
+    df3 = pd.DataFrame(y_resampled,columns=['simple_journal'])
+    df2 = df2.append(df3)
+
+    #write to csv file (takes a long time)
+    #print("Writing new Dataset!")
+    #df2.to_csv('data_for_student_case_SMOTE2.csv')
+    #print("Writing Dataset Finished!")
+
+    #check the number of data points for each lable
+    print(df2.groupby(['simple_journal']).size())
 
 def main():
     df = pd.read_csv('data_for_student_case.csv', index_col=0)  # dataframe
@@ -80,10 +88,10 @@ def main():
     df['amount'].apply(lambda x: float(x))
     # convert date to unix time
     df['creationdate'] = pd.DatetimeIndex(df['creationdate']).astype(np.int64) / 1000000000
-    df = df[df['shoppercountrycode'] != 'GB']
+    # df = df[df['shoppercountrycode'] != 'GB']
     df = df[df['simple_journal'] != 'Refused']
-    plots(df)
-    #smote(df)
+    #plots(df)
+    smote(df)
     #statistics(df)
 
 
