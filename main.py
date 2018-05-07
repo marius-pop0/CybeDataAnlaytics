03d39d24a89affa23ba560de479399a37173d3e9
marius <marius.pop94@gmail.com>
2018-05-07 23:52:05 +0200

    plot_amount_ave_diff

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
-from imblearn.over_sampling import smote
+from imblearn.over_sampling import SMOTE
 
 #format: txid,bookingdate,issuercountrycode,txvariantcode,bin,amount,currencycode,shoppercountrycode,shopperinteraction,simple_journal,cardverificationcodesupplied,cvcresponsecode,creationdate,accountcode,mail_id,ip_id,card_id
 
@@ -19,9 +19,9 @@ def preprocessing(df):
 def statistics(df):
     df1 = df.groupby(['shoppercountrycode', 'currencycode', 'simple_journal']).size().reset_index(name='freq').sort_values(by=['freq'], ascending=False).head()
     df2 = df[(df['shoppercountrycode'] == 'AU') & (df['currencycode'] == 'AUD')].groupby(['txvariantcode', 'simple_journal']).size().reset_index(name='freq').sort_values(by=['txvariantcode', 'simple_journal', 'freq'], ascending=False)
-    df3 = df[df['simple_journal'] == 'chargeback'].groupby(['shoppercountrycode', 'currencycode']).size().reset_index(name='freq').sort_values(by=['freq'], ascending=False)
-    df4 = df[(df['simple_journal'] == 'chargeback') & (df['shoppercountrycode']=='AU')].groupby(['card_id']).size().reset_index(name='freq').sort_values(by=['freq'], ascending=False).head(10)
-    df5 = df[df['simple_journal'] == 'settled'].groupby(['card_id']).size().reset_index(name='freq').sort_values(by=['freq'], ascending=False).head(20)
+    df3 = df[df['simple_journal'] == 'Chargeback'].groupby(['shoppercountrycode', 'currencycode']).size().reset_index(name='freq').sort_values(by=['freq'], ascending=False)
+    df4 = df[(df['simple_journal'] == 'Chargeback') & (df['shoppercountrycode']=='AU')].groupby(['card_id', ]).size().reset_index(name='freq').sort_values(by=['freq'], ascending=False).head(10)
+    df5 = df[df['simple_journal'] == 'Settled'].groupby(['card_id']).size().reset_index(name='freq').sort_values(by=['freq'], ascending=False).head(20)
     df6 = df[df['card_id'] == 'card182921'].groupby(['simple_journal', 'amount', 'creationdate', 'ip_id', 'currencycode', 'shopperinteraction', 'shoppercountrycode']).size().reset_index(name='freq').sort_values(by=['creationdate'])#.head(10)
 
 
@@ -34,10 +34,10 @@ def plot_time_diff(df):
 
     # df['AUD_currency'] = df[['currencycode', 'amount']].apply(lambda x: currencyconverter.convert_currency_from_aud(x), axis=1)
     df['date'] = pd.to_datetime(df['creationdate'])
-    df['diff_time'] = df.sort_values(['creationdate']).groupby(['card_id', 'date'])['date'].diff() #TODO: check!
+    df['diff_time'] = df.sort_values(['card_id', 'creationdate']).groupby('card_id')['date'].diff()
     print(df.sort_values(['card_id', 'date']).head(20))
     time = pd.DatetimeIndex(df['diff_time'])
-    df['diff_time_min'] = time.hour * 60 + time.minute + 1 # df['diff_time'].str.split(':').apply(lambda x: int(x[0]) * 60 + int(x[1]))
+    df['diff_time_min'] = time.hour * 60 + time.minute + 1  # df['diff_time'].str.split(':').apply(lambda x: int(x[0]) * 60 + int(x[1]))
     df['diff_time_min'] = df['diff_time_min'].fillna(0)
     # df.sort_values(['card_id', 'date']).head(20)
 
@@ -73,6 +73,32 @@ def plot_daily_freq(df):
 
     plt.show()
 
+def plot_amount_ave_diff(df):
+    #Y
+    df['date'] = pd.to_datetime(df['creationdate'])
+    df['ave_amount'] = df.sort_values(['creationdate']).groupby(['card_id'])['amount'].apply(pd.expanding_mean)
+    df['diff_from_ave_amount'] = df['amount'] - df['ave_amount']
+    #X
+    df['date'] = pd.to_datetime(df['creationdate'])
+    df['diff_time'] = df.sort_values(['card_id', 'creationdate']).groupby('card_id')['date'].diff()
+    print(df.sort_values(['card_id', 'date']).head(20))
+    time = pd.DatetimeIndex(df['diff_time'])
+    df['diff_time_min'] = time.hour * 60 + time.minute + 1  # df['diff_time'].str.split(':').apply(lambda x: int(x[0]) * 60 + int(x[1]))
+    df['diff_time_min'] = df['diff_time_min'].fillna(0)
+    print(df.head(20))
+
+    #plot
+    df2 = df[df['simple_journal'] == 'Chargeback']
+    df3 = df[df['simple_journal'] == 'Settled']
+    s = plt.scatter(df3['diff_time_min'], df3['diff_from_ave_amount'], s=4)
+    f = plt.scatter(df2['diff_time_min'], df2['diff_from_ave_amount'], s=4)
+    plt.legend((f, s), ('Fraud', 'Legitimate'))
+    plt.xlabel('time delta minutes')
+    plt.ylabel('amount delta from average')
+
+    plt.show()
+
+
 def plots(df):
     df['simple_journal'], labels = pd.factorize(df.simple_journal)
 
@@ -107,24 +133,24 @@ def smote(df):
                                      'shopperinteraction', 'accountcode', 'cardverificationcodesupplied'])
 
     y = df['simple_journal'].values
-    df = df.drop(['simple_journal', 'bookingdate', 'mail_id', 'ip_id', 'card_id'], axis=1)
-    x = df.values
+    df = df.drop(['simple_journal','creationdate','bookingdate', 'mail_id', 'ip_id', 'card_id'], axis=1)
+    X = df.values
 
-    print(np.shape(x), np.shape(y))
+    print(np.shape(X), np.shape(y))
 
 
-    # smote data
-    sm = smote(random_state=15)
-    x_resampled, y_resampled = sm.fit_sample(x, y)
+    # SMOTE Data
+    sm = SMOTE(random_state=15)
+    X_resampled, y_resampled = sm.fit_sample(X, y)
 
-    df2 = pd.DataFrame(x_resampled,columns=df.columns.values)
+    df2 = pd.DataFrame(X_resampled,columns=df.columns.values)
     df3 = pd.DataFrame(y_resampled,columns=['simple_journal'])
     df2 = df2.append(df3)
 
     #write to csv file (takes a long time)
-    #print("writing new dataset!")
-    #df2.to_csv('data_for_student_case_smote2.csv')
-    #print("writing dataset finished!")
+    #print("Writing new Dataset!")
+    #df2.to_csv('data_for_student_case_SMOTE2.csv')
+    #print("Writing Dataset Finished!")
 
     #check the number of data points for each lable
     print(df2.groupby(['simple_journal']).size())
@@ -134,13 +160,14 @@ def main():
     df = preprocessing(df)
     # convert date to unix time
     df['creationdate_unix'] = pd.DatetimeIndex(df['creationdate']).astype(np.int64) / 1000000000
-    # plots(df)
-    # smote(df)
-    # statistics(df)
+    #plots(df)
+    smote(df)
+    statistics(df)
     # print(df.columns.values)
 
-    # plot_time_diff(df)
+    #plot_time_diff(df)
     plot_daily_freq(df)
+    #plot_amount_ave_diff(df)
 
 
 
