46e07c184685490ae06e05888a4562c9e61ccc7a
Anna <annakosiorek4@gmail.com>
2018-05-24 14:54:58 +0200

    ass1 folder

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,3 +1,4 @@
+import itertools
 import math
 
 import matplotlib.pyplot as plt
@@ -7,10 +8,10 @@ import numpy as np
 
 from sklearn.metrics import confusion_matrix
 from imblearn.over_sampling import SMOTE
-from imblearn.combine import SMOTETomek
+from imblearn.combine import SMOTETomek, SMOTEENN
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix as sk_confusion_matrix
 from sklearn.metrics import roc_curve, auc
@@ -18,8 +19,11 @@ from sklearn.preprocessing import label_binarize
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import PCA, SparsePCA
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import KFold, StratifiedKFold, TimeSeriesSplit
 from sklearn import svm
+from imblearn.under_sampling import TomekLinks
+from sklearn.neighbors import KNeighborsClassifier
+
 
 import plotting as plotting
 from currency_converter import CurrencyConverter
@@ -33,22 +37,23 @@ def preprocessing(df, unix, diff):
     # df = df[df['shoppercountrycode'] != 'GB']
     df['cvcresponsecode'] = df['cvcresponsecode'].map(lambda x: float(x))
     df.loc[df['cvcresponsecode'] >= 3.0, 'cvcresponsecode'] = 3.0
+    df['cvcresponsecode'] = df['cvcresponsecode'].apply(lambda x: x if x <=2.0 else 3.0)
     df['amount'] = df['amount'].map(lambda x: float(x))
     df['card_id'] = df['card_id'].map(lambda x: x[4:])
     df['ip_id'] = df['ip_id'].map(lambda x: x[2:])
     df['mail_id'] = df['mail_id'].map(lambda x: x[5:])
-    min_unix = 0
+    df['country'] = df['shoppercountrycode'].apply(lambda x: x if x in ['MX', 'AU', 'GB'] else 'other')
+    df = df.drop(['shoppercountrycode'], axis=1)
     if unix:
         df['creationdate_unix'] = pd.DatetimeIndex(df['creationdate']).astype(np.int64) / 1000000000
-        min_unix = df['creationdate_unix'].min()
-        df['creationdate_unix'].sub(min_unix, axis=0)
+        df['creationdate_unix'].sub(df['creationdate_unix'].min(), axis=0)
         df['creationdate_unix'] = df['creationdate_unix'].map(lambda x: math.log(x))
     if diff:
         df = time_diff(df)
     df['AUD_currency'] = df[['currencycode', 'amount']].apply(lambda x: CurrencyConverter.convert_currency_from_AUD(x), axis=1)
     df.dropna(axis=0, how='any', inplace=True)
     df = df[~df.isin(['NaN', 'NaT', 'NA']).any(axis=1)]
-    return df, min_unix
+    return df
 
 def classification(X_train, y_train, X_test, classyType, cross_val = False):
     if classyType == 'NB':
@@ -59,13 +64,16 @@ def classification(X_train, y_train, X_test, classyType, cross_val = False):
         model = MLPClassifier() # -- good?
     elif classyType == 'DT':
         print('Decision Tree')
-        model = DecisionTreeClassifier() # -- good?
+        model = DecisionTreeClassifier(max_depth=10) # -- good?
     elif classyType == 'RF':
         print('Random Forest')
         model = RandomForestClassifier()
     elif classyType == 'LR':
         print('Linear Regression')
         model = LogisticRegression() # -- bad
+    elif classyType == 'KNN':
+        print('KNN')
+        model = KNeighborsClassifier()
 
     model.fit(X_train, y_train)
     predict_prob = model.predict_proba(X_test)[:, 1]
@@ -74,7 +82,7 @@ def classification(X_train, y_train, X_test, classyType, cross_val = False):
 
 
 def pca(X_train, X_test):
-    pca_model = PCA(n_components=5)# svd_solver='full')
+    pca_model = PCA(n_components=0.99, svd_solver='full')
     X_train = pca_model.fit_transform(X_train)
     X_test = pca_model.transform(X_test)
     print('PCA k={}'.format(np.shape(X_train)[1]))
@@ -111,28 +119,44 @@ def plot_ROC(y_test, smoted_predict_prob, unsmoted_predict_prob, smoted_predict_
     plt.show()
 
 def cross_validation(clsType, X, y, df):
-    k_fold = StratifiedKFold(10)
+    k_fold = StratifiedKFold(10, shuffle=True)#TimeSeriesSplit(n_splits=11)#StratifiedKFold(10)
     final_conf_matrix = np.zeros((2, 2))
-    if clsType == 'RF':
-        print('Random Forest')
-        model = RandomForestClassifier(class_weight='balanced')
-    elif clsType == 'SVM':
-        print('SVM')
-        model = svm.LinearSVC(class_weight='balanced')
     for k, (train, test) in enumerate(k_fold.split(X, y)):
-        print('Iteration {}'.format(k))
-        sm = SMOTETomek()  # SMOTE()# kind_smote='svm'
-        X_train, y_train = sm.fit_sample(X[train], y[train])
-        X_train = calc_time_diff(X_train, df)
+        if clsType == 'RF':
 
+            print('Random Forest')
+            model = RandomForestClassifier(n_estimators=100, max_depth=10)#class_weight='balanced')
+        elif clsType == 'SVM':
+            print('SVM')
+            model = svm.LinearSVC()#class_weight='balanced')
+        elif clsType == 'MLP':
+            print('MLP')
+            model = MLPClassifier()#class_weight='balanced')
+        elif clsType == 'NB':
+            print('NB')
+            model = GaussianNB()  # class_weight='balanced')
+        elif clsType == 'DT':
+            print('DT')
+            model = DecisionTreeClassifier(max_depth=10)
+        elif clsType == 'LR':
+            print('LR')
+            model = LogisticRegression()
+        print('Iteration {}'.format(k))
+        sm =  SMOTE()# kind_smote='svm' #TomekLinks()# SMOTETomek()  #
+        X_train, y_train = sm.fit_sample(X[train], y[train])
         X_test = X[test]; y_test = y[test]
-        X_test = calc_time_diff(X_test, df)
+
+        ######
+        # for oversampling to use time diff feature
+        # X_train = calc_time_diff(X_train, df)
+        # X_test = calc_time_diff(X_test, df)
+        ######
 
         # if 'creationdate_unix' in
         print('Train data size: {}'.format(np.shape(X_train)))
         print('Test data size: {}'.format(np.shape(X_test)))
         print('Test data fraud #{}'.format((y_test == 1).sum()))
-        X_train, X_test = pca(X_train, X_test)
+        # X_train, X_test = pca(X_train, X_test)
         model.fit(X_train, y_train)
         score = model.score(X_test, y_test)
         y_pred = model.predict(X_test)
@@ -152,11 +176,13 @@ def calc_time_diff(X, df):
     return X
 
 def time_diff(df):
+    # for undersampling
     df['date'] = pd.to_datetime(df['creationdate'])
+
     df['diff_time'] = df.sort_values(['card_id', 'creationdate']).groupby('card_id')['date'].diff()
-    # print(df.sort_values(['card_id', 'date']).head(20))
     time = pd.DatetimeIndex(df['diff_time'])
-    df['diff_time_min'] = time.hour * 60 + time.minute + 1  # df['diff_time'].str.split(':').apply(lambda x: int(x[0]) * 60 + int(x[1]))
+    df['diff_time_min'] = time.hour * 60 + time.minute + 1
+
     df['diff_time_min'] = df['diff_time_min'].fillna(0)
     df = df.drop(['date', 'diff_time'], axis=1)
     return df
@@ -165,15 +191,40 @@ def compute(df, clsType, crossVal):
     # df['creationdate'] = pd.to_datetime(df['creationdate'], unit='s')
 
     y = df['simple_journal'].values
-    # print(y)
     y = label_binarize(y, classes=['Settled', 'Chargeback'])
     y = np.concatenate(y, 0)
 
-    df = df.drop(['simple_journal', 'creationdate', 'bookingdate', 'amount'], axis=1)#, 'card_id' 'mail_id', 'ip_id'
+    df = df.drop(['simple_journal', 'creationdate', 'bookingdate', 'amount', 'shopperinteraction'], axis=1)#, 'card_id' 'mail_id', 'ip_id' 'amount
 
-    df = pd.get_dummies(df, columns=['issuercountrycode', 'txvariantcode', 'currencycode', 'shoppercountrycode',
-                                     'shopperinteraction', 'accountcode', 'cardverificationcodesupplied'])
-    # print(df.columns.values)
+
+    #one hot encoding -- used for ROC curves
+    # df = pd.get_dummies(df, columns=['issuercountrycode', 'txvariantcode', 'currencycode', 'country',
+    #                                  'shopperinteraction', 'accountcode', 'cardverificationcodesupplied'])
+
+    ######
+    #categorical into numerical -- used for ass3
+    categorical_cols = ['txvariantcode',
+                        'currencycode',
+                        'cardverificationcodesupplied',
+                        'cvcresponsecode',
+                        'accountcode',
+                        'country',
+                        'issuercountrycode']
+
+    # convert to categorical data
+    df[categorical_cols] = df[categorical_cols].apply(lambda x: x.astype('category'))
+
+    for col in categorical_cols:
+        df[col] = df[col].cat.codes
+    ######
+
+    print(df.columns.values)
+
+    # sorting values
+    # df = df.sort_values(by=['creationdate_unix'])
+    # drop if you don't want this feature
+    # df = df.drop(['creationdate_unix'], axis=1)
+    print(df.head())
 
     print('Amount of fraud data: {} and non fraud: {}'.format((y == 1).sum(), (y == 0).sum()))
     X = df.values
@@ -181,12 +232,6 @@ def compute(df, clsType, crossVal):
     if crossVal:
         cross_validation(clsType, X, y, df)
     else:
-        # length = np.shape(X)[0]#, np.shape(y))
-        # train_len = round(length*ratio)
-        # X_train = X[:train_len, :]
-        # y_train = y[:train_len]
-        # X_test = X[train_len:, :]
-        #     # y_test = y[train_len:]
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         print('Test data size: ' + str(np.shape(X_test)))
         print('Test data fraud #{}'.format((y_test == 1).sum()))
@@ -208,28 +253,18 @@ def compute(df, clsType, crossVal):
         print('smoted:   ' + str(smoted_model.score(X_test_sm, y_test)))
         print('unsmoted: ' + str(unsmoted_model.score(X_test_unsm, y_test)))
 
+        dotfile = open("dt.dot", 'w')
+        export_graphviz(smoted_model, out_file=dotfile, feature_names=df.columns.values)
+        dotfile.close()
+
         plot_ROC(y_test, smoted_predict_prob, unsmoted_predict, smoted_predict_bin, unsmoted_predict_bin)
-
-    # return X_resampled, y_resampled, X_test, y_test
-
-    # df2 = pd.DataFrame(X_resampled,columns=df.columns.values)
-    # df3 = pd.DataFrame(y_resampled,columns=['simple_journal'])
-    # df2 = df2.append(df3)
-
-    #write to csv file (takes a long time)
-    #print("Writing new Dataset!")
-    #df2.to_csv('data_for_student_case_SMOTE2.csv')
-    #print("Writing Dataset Finished!")
-
-    #check the number of data points for each lable
-    # print(df2.groupby(['simple_journal']).size())
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
     # General commands
     # parser.add_argument('plot', help='Plot', action='store_true')
-    parser.add_argument('cls', help='Classifier type', action='store', choices=['NB', 'MLP', 'DT', "RF", 'SVM', 'LR'], default='default')
+    parser.add_argument('cls', help='Classifier type', action='store', choices=['NB', 'MLP', 'DT', "RF", 'SVM', 'LR', 'KNN'], default='default')
 
     args = parser.parse_args()
     return args
@@ -237,39 +272,19 @@ def parse_arguments():
 
 def run(cls, unix, diff):
     df = pd.read_csv('data_for_student_case.csv', index_col=0)  # dataframe
-    df, min_unix = preprocessing(df, unix, diff)
+    df = preprocessing(df, unix, diff)
+    # true for cross validation
     compute(df, cls, True)
 
+
 def main():
-
-    df = pd.read_csv('data_for_student_case.csv', index_col=0)  # dataframe
-    # df = preprocessing(df)
-
     args = parse_arguments()
-
-    # convert date to unix time
-    # plotting.plots(df)
-    # plotting.statistics(df)
-    # print(df.columns.values)
-
-    # plotting.plot_time_diff(df)
-    # plotting.plot_daily_freq(df)
-    # plotting.plot_amount_ave_diff(df)
-    # plotting.plot_cards_per_mail(df)
-    # plotting.plot_cards_per_ip(df)
 
     if args.cls is not None:
         print('1')
         run(args.cls, True, False)
-        # print('2')
-        # run(args.cls, False, True)
-        # print('3')
-        # run(args.cls, False, False)
-        # print('4')
+        print('2')
         # run(args.cls, True, True)
-
-        # compute(df, args.cls, True)
-
 
 
 main()
