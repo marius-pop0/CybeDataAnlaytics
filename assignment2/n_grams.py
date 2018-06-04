c86b7758272460153cc30e4bf371e65fc27d27c1
Anna <annakosiorek4@gmail.com>
2018-06-04 16:07:57 +0200

    utils

--- assignment2/n_grams.py
+++ assignment2/n_grams.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import utils
 from sax import SAX
 
 def n_gram_model(n_grams):
@@ -49,50 +50,79 @@ def main():
     train2_df = pd.read_csv('BATADAL_dataset04.csv', index_col=0)
     # test_df = pd.read_csv('BATADAL_test_dataset.csv', index_col=0)
     train1_df.index = pd.to_datetime(train1_df.index, dayfirst=True)
-    for col in ['L_T1', 'L_T4', 'L_T7', 'S_PU10', 'S_PU11', 'F_PU10', 'F_PU11', 'S_PU2', 'S_PU6', 'F_PU2', 'F_PU6', 'F_PU7']:
-        for window_size in [10]:
-            for word_size in [3]:
-                alphabet_size = 3
-                stride = 1
-                sax = SAX(wordSize=word_size, alphabetSize=alphabet_size)
+    labels = []
+    train2_df['n_gram'] = np.zeros(len(train2_df))
+    for col in ['L_T1',  'F_PU11',  'S_PU6']: #'L_T4', 'L_T7', 'S_PU10', 'S_PU11', 'F_PU10', 'F_PU2', 'F_PU6', 'F_PU7 'S_PU2',
+        window_size = 10
+        word_size = 3
+        alphabet_size = 3
+        stride = 1
+        sax = SAX(wordSize=word_size, alphabetSize=alphabet_size)
 
-                # for column_name in train1_df:
-                train_string_rep, train_window_indices = sax.sliding_window(train1_df[col].values, cover=window_size,
-                                                                            stride=stride)
-                train_string_rep2, train_window_indices2 = sax.sliding_window(train2_df[col].values, cover=window_size,
-                                                                              stride=stride)
+        # for column_name in train1_df:
+        train_string_rep, train_window_indices = sax.sliding_window(train1_df[col].values, cover=window_size,
+                                                                    stride=stride)
+        train_string_rep2, train_window_indices2 = sax.sliding_window(train2_df[col].values, cover=window_size,
+                                                                      stride=stride)
 
-                for threshold in [1e-6]:
+        threshold = 1e-6
 
-                    # print(train_string_rep)
-                    # print(train_window_indices)
+        # print(train_string_rep)
+        # print(train_window_indices)
 
-                    # print(np.shape(train_string_rep2))
+        # print(np.shape(train_string_rep2))
 
-                    model = n_gram_model(train_string_rep)
-                    anomalies, probabilities = n_gram_predict(model, train_string_rep2, train_window_indices2, threshold, window_size)
-                    print('window: {}, word: {}, alphabet: {}, threshold: {}'.format(window_size, word_size, alphabet_size, threshold))
+        model = n_gram_model(train_string_rep)
+        anomalies, probabilities = n_gram_predict(model, train_string_rep2, train_window_indices2, threshold, window_size)
+        print('window: {}, word: {}, alphabet: {}, threshold: {}'.format(window_size, word_size, alphabet_size, threshold))
 
-                    # print(anomalies)
+        # print(anomalies)
 
-                    # print(np.shape(probabilities))
-                    # print(np.shape(train2_df.values))
-                    plt.clf()
-                    train2_df['ATT_FLAG_anom'] = np.where(train2_df['ATT_FLAG'] == 1, 1.5, 0)
-                    ax = train2_df['ATT_FLAG_anom'].plot(grid=True, color='r', label='Anomaly')
-                    train2_df['n_gram'] = probabilities
+        # print(np.shape(probabilities))
+        # print(np.shape(train2_df.values))
+        plt.clf()
+        train2_df['ATT_FLAG_anom'] = np.where(train2_df['ATT_FLAG'] == 1, 100, 0)
+        # ax = train2_df['ATT_FLAG_anom'].plot(grid=True, color='r', label='Anomaly')
+        # labels += probabilities
+        train2_df['n_gram'] += probabilities
 
-                    ax2 = train2_df['n_gram'].plot(grid=True, label='Validation')
 
-                    plt.legend()
-                    plt.title('window: {} threshold: {}, col:{}'.format(window_size, threshold, col))
-                    plt.savefig('images/fig_{}_{}_{}.png'.format(window_size, threshold, col))
-                    # plt.show()
-    #
+
+        # ax2 = train2_df['n_gram'].plot(grid=True, label='Validation')
+        #
+        # plt.legend()
+        # plt.title('window: {} threshold: {}, col:{}'.format(window_size, threshold, col))
+        # plt.savefig('images/fig_{}_{}_{}.png'.format(window_size, threshold, col))
+        # plt.show()
+#
     # plt.plot(probabilities, '.')
     # plt.show()
 
+
     # model = NgramModel(3, train_string_rep)
     # perplexity = model.perplexity(train_string_rep2)
+
+    train2_df['n_gram'] =  np.where(train2_df['n_gram'] > 0, 1, 0)
+
+    train2_df['diff'] = train2_df['ATT_FLAG_anom'] - train2_df['n_gram']
+
+    arr = train2_df['diff'].value_counts()
+    print(arr)
+
+    TTD = utils.TDD_metric(train2_df, probabilities)
+    TP = arr[99]
+    FP = arr[-1]
+    TN = arr[0]
+    FN = arr[100]
+    S_CM = utils.S_cm(TP, FP, TN, FN)
+
+    accuracy = (TP+TN)/len(train2_df)
+    precision = TP/(TP+FP)
+
+    print('accuracy: {}, precision: {}'.format(accuracy, precision))
+    print('TDD: {}'.format(TTD))
+    print('S_cm: {}'.format(S_CM))
+    print('Ranked: {}'.format(0.5 * TTD + 0.5 * S_CM))
+
 
 main()
---
+++ assignment2/utils.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+
+def TDD_metric(train2_df, y_pred):
+    counter = 0
+    found_c = 0
+    bit = 0
+    counter_list = []
+    for rindex, row in train2_df.iterrows():
+        if row['ATT_FLAG'] == 1:
+            if bit == 0:
+                counter = 0
+                bit = 1
+                found_c += 1
+            counter += 1
+        else:
+            if bit == 1:
+                for c in range(counter):
+                    counter_list.append((c + 1) / counter)
+                bit = 0
+            counter_list.append(0)
+
+    train2_df['TDD'] = counter_list
+    train2_df['y_pred'] = y_pred
+
+    counter = 0
+    found_c = 0
+
+    arr = train2_df[['ATT_FLAG', 'TDD']].values
+    for i in range(len(arr)):  # rindex, row in train2_df.iterrows():
+        if arr[i][0] == 1:
+            if y_pred[i] == 1 and bit == 0:
+                #                 print(arr[i][1])
+                counter += arr[i][1]
+                bit = 1
+                found_c += 1
+        else:
+            bit = 0
+            #     print('A')
+    #     print(counter, found_c)
+    S_TDD = 1 - counter / found_c
+    return S_TDD
+
+
+def S_cm(TP, FP, TN, FN):
+    TPR = TP/(TP + FN)
+    TNR = TN/(FP + TN)
+    return (TPR + TNR)/2
